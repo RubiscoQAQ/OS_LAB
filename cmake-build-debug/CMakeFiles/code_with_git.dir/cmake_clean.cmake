01b3a56cefe9982795acf3454c1ab0ddbe38ed95
Administrator <admin@example.com>
2021-11-03 17:40:30 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/code_with_git.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/code_with_git.dir/main.cpp.obj"
+  "code_with_git.exe"
+  "code_with_git.exe.manifest"
+  "code_with_git.lib"
+  "code_with_git.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/code_with_git.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
